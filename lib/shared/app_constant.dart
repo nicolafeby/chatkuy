62b050e18ae7610ccb94c023166fba8de20b7808
Muhammad Nicola Feby Salvaturi <muhammad.salvaturi@MAS001NIT42.local>
2024-01-20 16:19:56 +0700

    feature: create login screen

---
+++ lib/shared/app_constant.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppConstant {
+  static const String appId = '1:682810797194:web:c4e37e48d343f48bcd5173';
+  static const String apiKey = 'AIzaSyBlQNyNOSvongMxAzJLsqA7fe4qXMH8K-o';
+  static const String messagingSenderId = '682810797194';
+  static const String projectId = 'chatkuy-3f559';
+}
+
+class AppColor {
+  static const Color primaryColor = Color(0xFFEE7B64);
+}
